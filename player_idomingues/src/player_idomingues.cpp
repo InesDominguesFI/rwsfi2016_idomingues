2b800e9fd97b214a74cf1b8785c507f00257d5ae
tiagoesteves <tiagoesteves@followinspiration.pt>
2016-12-21 15:23:32 +0000

    mais uma vez!

--- player_idomingues/src/player_idomingues.cpp
+++ player_idomingues/src/player_idomingues.cpp
@@ -31,20 +31,53 @@ class MyPlayer: public rwsfi2016_libs::Player
     {
       //Custom play behaviour. Now I will win the game
 
+	double distance_to_arena = getDistanceToArena();
+
       //Behaviour follow the closest prey
       //move(msg.max_displacement, getAngleToPLayer(preys_team->players[0]));
       //move(msg.max_displacement, M_PI/30);
-	double dist_min = INFINITY; int pl_min=0;
-	for(int pl=0;pl<preys_team->players.size();pl++)
+	double dist_to_preys_min = INFINITY; 
+	int chosen_pl_to_hunt=0;
+	int chosen_pl_to_run_away_from=0;
+	double min_admissible_dist_to_hunters = 1; 
+	double displacement = msg.max_displacement;
+	bool escape = 0;
+	for(int pl_prey=0;pl_prey<preys_team->players.size();pl_prey++)
 	{
-		double dist = getDistanceToPlayer(preys_team->players[pl]);
-		if (dist<dist_min)
+		escape = 0;
+		double dist_to_prey = getDistanceToPlayer(preys_team->players[pl_prey]);
+		if (dist_to_prey<dist_to_preys_min)
 		{
-			dist_min = dist; 
-			pl_min = pl;
+			for(int pl_hunter=0;pl_hunter<hunters_team->players.size();pl_hunter++)
+			{
+				double dist_to_hunter = getDistanceToPlayer(hunters_team->players[pl_hunter]);
+				if (dist_to_hunter < min_admissible_dist_to_hunters)
+				{
+					escape = 1; 
+					chosen_pl_to_run_away_from =  pl_hunter;
+					break;
+				}
+			}
+			dist_to_preys_min = dist_to_prey; 
+			chosen_pl_to_hunt = pl_prey;
 		} 
 	}
-	move(msg.max_displacement,getAngleToPLayer(preys_team->players[pl_min]));
+	if (distance_to_arena < 8)
+	{
+		if (escape == 0)
+		{
+			move(msg.max_displacement,getAngleToPLayer(preys_team->players[chosen_pl_to_hunt]));
+		}
+		else
+		{
+			move(msg.max_displacement,-getAngleToPLayer(hunters_team->players[chosen_pl_to_run_away_from]));
+		}
+	}
+	else 
+	{
+		string arena = "/map";
+		move(msg.max_displacement, getAngleToPLayer(arena) );
+	}
     }
 };
 
